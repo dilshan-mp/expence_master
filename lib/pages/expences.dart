066e5f2480f899babe558fbfb18891702362e4dd
IT21185298 <it21185298@my.sliit.lk>
2024-01-19 21:57:16 +0530

    expences page create

---
+++ lib/pages/expences.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Expences extends StatefulWidget {
+  const Expences({super.key});
+
+  @override
+  State<Expences> createState() => _ExpencesState();
+}
+
+class _ExpencesState extends State<Expences> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
