b543c2b01956e76c96bc178ab6d40ff8488895c8
IT21185298 <it21185298@my.sliit.lk>
2024-01-19 22:46:07 +0530

    add uui dependenci

--- lib/models/expence.dart
+++ lib/models/expence.dart
@@ -1,14 +1,17 @@
 
 enum Category{food,travel,leasure,work}
 
+//constructor 
 class ExpenceModel{
   ExpenceModel( {
     required this.amount,
     required this.date,
     required this.title,
     required this.category
-  });
-
+  })
+  :id = "001";
+  
+  final String id;
   final String title;
   final double amount;
   final DateTime date;
